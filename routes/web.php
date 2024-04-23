a0a7d984353fb9459ae8ff51f9720cbd4de5fdbb
dinoop.k <dinoop.k@bizfab.com>
2024-04-23 17:20:38 +0530

    package json update for build

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::get('/test', [TestController::class, 'test']);
+Route::get("{any}", function () {
+    return view('welcome');
+})->where('any', '.*');
